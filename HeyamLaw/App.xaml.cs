3fed80f16455f3fc8b772cc38426885256f05888
Heydar Mohammadi nasir <heydarmohammadinasir@outlook.com>
2025-07-30 19:06:11 +0330

    Add MAUI For Mobile 1404/05/08

---
+++ HeyamLaw/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HeyamLaw
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
